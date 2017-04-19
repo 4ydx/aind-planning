813a08bcd5eef8753eea8814821682ba2112e07a
Nathan Findley <nathan@4ydx.com>
2017-04-19 17:49:02 +0900

    Mutex complete.  Test passes.

--- my_planning_graph.py
+++ my_planning_graph.py
@@ -501,8 +501,11 @@ class PlanningGraph():
         :param node_s2: PgNode_s
         :return: bool
         '''
-        # TODO test for Inconsistent Support between nodes
-        return False
+        for action_a1 in node_s1.parents:
+            for action_a2 in node_s2.parents:
+                if not action_a1.is_mutex(action_a2):
+                    return False
+        return True
 
     def h_levelsum(self) -> int:
         '''The sum of the level costs of the individual goals (admissible if goals independent)
